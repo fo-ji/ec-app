4007465ba3324fe7d7f5b20d6d1282e0bbb5b172
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-10-09 00:10:23 +0900

    Reducersのサンプル2

---
+++ src/reducks/users/reducers.js
@@ -0,0 +1,11 @@
+export const UsersReducer = (state = initialState.users, action) => {
+  switch (action.type) {
+    case Actions.SIGN_IN:
+      return {
+        ...state,
+        ...action.payload
+      }
+    default:
+      return state
+  }
+}
