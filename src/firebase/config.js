ab3cf56ddf7b79c63386dc1fb5d8faa86ffd26a8
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-10-11 11:57:35 +0900

    共通のTextInputコンポーネントの実装

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAJ0aC5Q7IyBaHQshLFgK2f7BiBg60dNOY",
+  authDomain: "ec-app-90db7.firebaseapp.com",
+  databaseURL: "https://ec-app-90db7.firebaseio.com",
+  projectId: "ec-app-90db7",
+  storageBucket: "ec-app-90db7.appspot.com",
+  messagingSenderId: "113444636401",
+  appId: "1:113444636401:web:58cd95fccd8ee5b5dc8b50",
+  measurementId: "G-H2G25ZLSG6"
+};
