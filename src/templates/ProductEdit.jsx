3a8604f57c97030b69a07bf754751731317ad658
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-10-15 23:28:58 +0900

    カテゴリ、性別のフォーム作成

--- src/templates/ProductEdit.jsx
+++ src/templates/ProductEdit.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useCallback} from "react";
-import { TextInput } from "../components/UIkit";
+import { TextInput, SelectBox, PrimaryButton } from "../components/UIkit";
 
 const ProductEdit = () => {
 
@@ -21,6 +21,18 @@ const ProductEdit = () => {
     setPrice(e.target.value)
   }, [setPrice])
 
+  const categories = [
+    {id: "tops", name: "トップス"},
+    {id: "shirts", name: "シャツ"},
+    {id: "pants", name: "パンツ"},
+  ];
+
+  const genders = [
+    {id: "all", name: "すべて"},
+    {id: "mens", name: "メンズ"},
+    {id: "female", name: "レディース"},
+  ];
+
   return (
     <section>
       <h2 className="u-text__headline u-text-center">商品の登録・編集</h2>
@@ -33,10 +45,23 @@ const ProductEdit = () => {
           fullWidth={true} label={"商品説明"} multiline={true} required={true}
           onChange={inputDescription} rows={5} value={description} type={"text"}
         />
+        <SelectBox
+          label={"カテゴリー"} required={true} options={categories} select={setCategory} value={category}
+        />
+        <SelectBox
+          label={"性別"} required={true} options={genders} select={setGender} value={gender}
+        />
         <TextInput
           fullWidth={true} label={"価格"} multiline={false} required={true}
           onChange={inputPrice} rows={1} value={price} type={"number"}
         />
+        <div className="module-spacer--medium" />
+        <div className="center">
+          <PrimaryButton
+            label={"商品情報を保存"}
+            onClick={}
+          />
+        </div>
       </div>
     </section>
   )
