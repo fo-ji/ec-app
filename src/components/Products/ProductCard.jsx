6e4afd5ae4abd376e6cd7cad59749e23adf206e7
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-10-18 22:17:18 +0900

    商品の一覧からの削除実装

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -11,6 +11,7 @@ import IconButton from "@material-ui/core/IconButton";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { deleteProduct } from "../../reducks/products/operations";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -92,7 +93,12 @@ const ProductCard = (props) => {
           >
             編集する
           </MenuItem>
-          <MenuItem>
+          <MenuItem
+            onClick={() => {
+              dispatch(deleteProduct(props.id));
+              handleClose();
+            }}
+          >
             削除する
           </MenuItem>
         </Menu>
