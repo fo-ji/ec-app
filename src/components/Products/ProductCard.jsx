64ee4e548efa4447fef2b0c8c9662f0b8cc3df16
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-10-18 21:53:25 +0900

    商品の編集・削除メニューの実装

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -7,6 +7,10 @@ import Typography from '@material-ui/core/Typography';
 import NoImage from '../../assets/img/src/no_image.png'
 import { push } from "connected-react-router";
 import { useDispatch } from "react-redux";
+import IconButton from "@material-ui/core/IconButton";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -41,6 +45,16 @@ const ProductCard = (props) => {
   const classes = useStyles();
   const dispatch = useDispatch();
 
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  };
+
   const images = (props.images.length > 0) ? props.images : [{path: NoImage}];
   const price = props.price.toLocaleString();
 
@@ -61,6 +75,27 @@ const ProductCard = (props) => {
             ¥{price}
           </Typography>
         </div>
+        <IconButton onClick={handleClick}>
+          <MoreVertIcon />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem
+            onClick={() => {
+              dispatch(push("/product/edit/" + props.id))
+              handleClose()
+            }}
+          >
+            編集する
+          </MenuItem>
+          <MenuItem>
+            削除する
+          </MenuItem>
+        </Menu>
       </CardContent>
     </Card>
   )
