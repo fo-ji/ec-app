008b1aab85b5ca4ce2f264ddfc2d426f8d7d477a
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-10-18 10:44:04 +0900

    商品情報編集機能の実装

--- src/templates/ProductEdit.jsx
+++ src/templates/ProductEdit.jsx
@@ -1,11 +1,17 @@
-import React, {useState, useCallback} from "react";
+import React, {useState, useCallback, useEffect} from "react";
 import { useDispatch } from "react-redux";
 import { TextInput, SelectBox, PrimaryButton } from "../components/UIkit";
 import {saveProduct} from "../reducks/products/operations";
 import ImageArea from "../components/Products/ImageArea";
+import { db } from "../firebase/index";
 
 const ProductEdit = () => {
   const dispatch = useDispatch();
+  let id = window.location.pathname.split("/product/edit")[1];
+
+  if (id !== "") {
+    id = id.split("/")[1];
+  }
 
   const [name, setName]               = useState(""),
         [description, setDescription] = useState(""),
@@ -38,6 +44,21 @@ const ProductEdit = () => {
     {id: "female", name: "レディース"},
   ];
 
+  useEffect(() => {
+    if (id !== "") {
+      db.collection("products").doc(id).get()
+        .then(snapshot => {
+          const data = snapshot.data();
+          setImages(data.images);
+          setName(data.name);
+          setDescription(data.description);
+          setCategory(data.category);
+          setGender(data.gender);
+          setPrice(data.price);
+        })
+    }
+  }, [id]);
+
   return (
     <section>
       <h2 className="u-text__headline u-text-center">商品の登録・編集</h2>
@@ -65,7 +86,7 @@ const ProductEdit = () => {
         <div className="center">
           <PrimaryButton
             label={"商品情報を保存"}
-            onClick={() => dispatch(saveProduct(name, description, category, gender, price, images))}
+            onClick={() => dispatch(saveProduct(id, name, description, category, gender, price, images))}
           />
         </div>
       </div>
