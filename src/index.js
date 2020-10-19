21e1104c47e041ac7ef7a891346d977a96603c32
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-10-19 23:34:39 +0900

    商品詳細ページの枠組みの実装

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 import createStore from "./reducks/store/store";
-import {ConnectedRouter} from "connected-react-router";
+import { ConnectedRouter } from "connected-react-router";
 import * as History from "history";
+import { MuiThemeProvider } from "@material-ui/core";
+import { theme } from "./assets/theme";
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
 
@@ -13,7 +15,9 @@ export const store = createStore(history);
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <App />
+      <MuiThemeProvider theme={theme}>
+        <App />
+      </MuiThemeProvider>
     </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
