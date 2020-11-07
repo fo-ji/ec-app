83b5e761ad3291f6db55ff9275a676322584ed8a
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-11-07 16:28:42 +0900

    OrderedProductsの実装

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -3,6 +3,7 @@ export { default as ImageArea } from "./ImageArea";
 export { default as ImageSwiper } from "./ImageSwiper";
 export { default as ImagePreview } from "./ImagePreview";
 export { default as OrderHistoryItem } from "./OrderHistoryItem";
+export { default as OrderedProducts } from "./OrderedProducts";
 export { default as SizeTable } from "./SizeTable";
 export { default as SetSizeArea } from "./SetSizeArea";
 export { default as ProductCard } from "./ProductCard";
