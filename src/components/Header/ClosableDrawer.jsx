e823d02fef0aafffd1ddadeba5aea10cc0dba6e5
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-11-09 23:53:30 +0900

    drawerメニューにカテゴリー、クエリを追加する

--- src/components/Header/ClosableDrawer.jsx
+++ src/components/Header/ClosableDrawer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react"
+import React, { useState, useCallback, useEffect } from "react"
 import { useDispatch } from "react-redux"
 import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
@@ -16,6 +16,7 @@ import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import { TextInput } from "../UIkit";
 import { push } from "connected-react-router"
 import { sighOut, signOut } from "../../reducks/users/operations"
+import { db } from "../../firebase";
 
 const useStyles = makeStyles((theme) => ({
   drawer: {
@@ -50,12 +51,34 @@ const ClosableDrawer = (props) => {
     dispatch(push(path))
     props.onClose(e)
   }
+  
+  const [filters, setFilters] = useState([
+    {func: selectMenu, label: "すべて", id: "all", value: "/"},
+    {func: selectMenu, label: "メンズ", id: "male", value: "/?gender=male"},
+    {func: selectMenu, label: "レディース", id: "female", value: "/?gender=female"}
+  ])
 
   const menus = [
     {func: selectMenu, label: "商品登録", icon: <AddCircleIcon />, id: "register", value: "/product/edit"},
     {func: selectMenu, label: "注文履歴", icon: <HistoryIcon />, id: "history", value: "/order/history"},
     {func: selectMenu, label: "プロフィール", icon: <PersonIcon />, id: "profile", value: "/user/mypage"},
   ]
+
+  useEffect(() => {
+    db.collection("categories")
+      .orderBy("order", "asc")
+      .get()
+      .then(snapshots => {
+        const list = []
+        snapshots.forEach(snapshot => {
+          const category = snapshot.data()
+          list.push({
+            func: selectMenu, label: category.name, id: category.id, value: `/?category=${category.id}`
+          })
+        })
+        setFilters(prevState => [...prevState, ...list])
+      })
+  }, [])
 
   return (
     <nav className={classes.drawer}>
@@ -96,6 +119,18 @@ const ClosableDrawer = (props) => {
               </ListItemIcon>
               <ListItemText primary={"Logout"} />
             </ListItem>
+          </List>
+          <Divider />
+          <List>
+          {filters.map(filter => (
+            <ListItem
+              button
+              key={filter.id}
+              onClick={(e) => filter.func(e, filter.value)}
+            >
+              <ListItemText primary={filter.label} />
+            </ListItem>
+          ))}
           </List>
         </div>
       </Drawer>
